e76ad275e5d41cf77b65ffd037b5d4b387742ae3
Vo Anh Dinh <vadinh@codixfr.private>
2019-12-14 16:15:21 +0700

    Reponsive Menu

---
+++ src/app/shared/services/CarService.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+// import { Car } from '../domain/car';
+
+export interface Car {
+    vin?;
+    year?;
+    brand?;
+    color?;
+    price?;
+    saleDate?;
+}
+
+@Injectable()
+export class CarService {
+
+    constructor(private http: HttpClient) { }
+
+    getCarsSmall() {
+    return this.http.get<any>('assets/showcase/data/cars-small.json')
+      .toPromise()
+      .then(res => <Car[]>res.data)
+      .then(data => { return data; });
+    }
+
+    getCarsMedium() {
+    return this.http.get<any>('assets/showcase/data/cars-medium.json')
+      .toPromise()
+      .then(res => <Car[]>res.data)
+      .then(data => { return data; });
+    }
+
+    getCarsLarge() {
+    return this.http.get<any>('assets/showcase/data/cars-large.json')
+      .toPromise()
+      .then(res => <Car[]>res.data)
+      .then(data => { return data; });
+    }
+
+  getCarsHuge() {
+    return this.http.get<any>('assets/showcase/data/cars-huge.json')
+      .toPromise()
+      .then(res => <Car[]>res.data)
+      .then(data => { return data; });
+  }
+}
