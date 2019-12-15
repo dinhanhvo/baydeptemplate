371ea5ac30e373c47ab5573f71547a5bd617da76
Vo Anh Dinh <vadinh@codixfr.private>
2019-12-15 11:28:00 +0700

    Routing for child

---
+++ src/app/layout/products/products-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductsComponent,
+    children: [
+      { path: '', redirectTo: 'products', pathMatch: 'prefix' },
+      { path: 'products', component: ProductsComponent },
+      { path: 'products/:id', component: ProductDetailsComponent }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProductsRoutingModule { }
---
+++ src/app/layout/products/products.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [ProductsComponent, ProductDetailsComponent],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    NgbModule
+  ]
+})
+export class ProductsModule { }
